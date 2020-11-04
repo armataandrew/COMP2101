7a47a483ce6c489cffce6074901efd5f1c4f3ec3
andrew <armataandrew@yahoo.com>
2020-11-03 20:15:28 -0800

    lab2_challange3

--- bash/improveddice.sh
+++ bash/improveddice.sh
@@ -21,14 +21,14 @@ minimum=1
 # Tell the user we have started processing
 echo "rolling with variables..."
 #rolls the first die
-roll=$(( RANDOM % sides + minimum))
+roll=$(( RANDOM % $sides + $minimum))
 #rolls the seccond die
-roll2=$(( RANDOM % sides + minimum))
+roll2=$(( RANDOM % $sides + $minimum))
 # Tell the user the results of the roll
 echo "This is your roll for die one: $roll and die two: $roll2 "
 # finds the sum of the numbers and assighns it to a variable
 sum=$(( $roll + $roll2))
 # finds the aavridge of the mumbers and assighns it a variable
 avrg=$(( sum/2))
-# Tell the user the results of the roll
+# Tell the user the results of the roll, the avrage and the sum.
 echo "This is your roll for die one: $roll and die two: $roll2, The sum of your rolls is $sum and the average of your rolls is $avrg"
